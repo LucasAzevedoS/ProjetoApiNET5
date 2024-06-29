8207501959ec6b9a6134a3ebce69b91814bbd6a4
Lucas Azevedo de Souza <lucas.a.souza@hotmail.com>
2024-06-28 21:00:17 -0300

    Add project files.

---
+++ ProjetoApiNET5/Controllers/UsuariosController.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace ProjetoApiNET5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet("get-usuarios")]
+        public List<usuarios> GetUsuarios()
+        {
+            var query = @"select * from usuarios";
+
+            using (var conn = new SqlConnection("Server=PC-LUCAS; Database=BANCO01; User Id=sa; Password=Lucas30092004;"))
+            {
+                conn.Open();
+
+                var resp = conn.Query<usuarios>(query).ToList();
+
+                return resp;
+            }
+        }
+
+        [HttpGet("get-usuarios-id/{id}")]
+        public List<usuarios> GetUsuariosId(int id)
+        {   
+            var query = @"select * from usuarios where id = @Id ";
+
+            using (var conn = new SqlConnection("Server=PC-LUCAS; Database=BANCO01; User Id=sa; Password=Lucas30092004;"))
+            {
+                conn.Open();
+
+                var resp = conn.Query<usuarios>(query, new { Id = id }).ToList();
+
+                return resp;
+            }
+        }
+
+    }
+}
---
+++ ProjetoApiNET5/usuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoApiNET5
+{
+    public class usuarios
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int idade { get; set; }
+        public DateTime dt_cadastro { get; set; }
+    }
+}
